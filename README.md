68c05888ea2b6f7c5b95f3c562f49665b57b582f
Boris Musatov <boris.musatov@gmail.com>
2016-12-03 18:58:43 +0100

    Set up Flyway for tests

---
+++ README.md
@@ -0,0 +1,5 @@
+Database layer testing:
+1. Init with Flyway migration
+2. Start Docker container with database
+3. Apply migration ( need to get connection from conteiner )
+4. Run tests
--- src/test/java/cz/linkskeeper/users/repository/UserRepositoryTest.java
+++ src/test/java/cz/linkskeeper/users/repository/UserRepositoryTest.java
@@ -2,34 +2,38 @@ package cz.linkskeeper.users.repository;
 
 import cz.linkskeeper.app.LinksKeeperApplication;
 import cz.linkskeeper.users.domain.User;
-import org.junit.Assert;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = {LinksKeeperApplication.class})
-@Profile("test")
 public class UserRepositoryTest {
+
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    public void shouldFindByUserName() {
-        User user = new User();
-        user.setFirstName("Joht");
-        user.setLastName("Smith");
-        user.setUserName("smithy");
+    public void shouldFindSomeUsers() {
+        List<User> users = userRepository.findAll();
+        assertThat(users.size()).isNotEqualTo(0);
+    }
 
-        userRepository.save(user);
-
-        User anotherUser = userRepository.findByUserName("smithy");
-
-        Assert.assertEquals(anotherUser.getUserName(), user.getUserName());
+    @Test
+    public void shouldFindUserByUsername() {
+        User user = userRepository.findByUserName("boris");
+        assertThat(user).isNotNull();
+        assertThat(user.getFirstName()).isEqualTo("Boris");
+        assertThat(user.getLastName()).isEqualTo("Musatov");
     }
 }
